9d609d1c35f81c3cbc1af9c8893efdf5bd6961dd
Q-roo <kovacs113istvan@gamil.com>
2024-09-19 02:14:35 +0900

    implement wm

--- Scripts/Language/Chartbuild/WM/CBWM.cs
+++ Scripts/Language/Chartbuild/WM/CBWM.cs
@@ -1,12 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using LanguageExt.UnsafeValueAccess;
 
 namespace PCE.Chartbuild.Runtime;
 
-public class WM {
-    const uint maxVariables = 255;
+using Address = UInt16;
 
+public class WM {
+    private const byte addressSizeInBytes = sizeof(Address) / sizeof(byte);
     private readonly byte[] byteCode;
-    private readonly CBVariable[] variables = new CBVariable[maxVariables];
+    private readonly CBVariable[] variables;
+    private readonly Stack<ICBValue> stack= new(200); // this much should be enough to start with
+
+    // when returning form a function, go to the previous location
+    // this is where those are stored
+    // I am probably doing this wrong but as long as it works...
+    private readonly Stack<uint> gotoStack= new(20); // this much should pe plenty as long as someone doesn't go nuts with the language
 
     private uint programCounter = 0;
 
@@ -14,19 +24,123 @@ public class WM {
         if (byteCode.Length == 0)
             return;
 
-            byte a = 255;
-
         for (; ; ) {
-            switch (Read()) {
-                case (byte)OpCode.Halt:
+            switch ((OpCode)Read()) {
+                case OpCode.Halt:
                     return;
+                case OpCode.Push:
+                    Push();
+                    break;
+                case OpCode.Pop:
+                    Pop();
+                    break;
+                case OpCode.Goto:
+                    Goto(true);
+                    break;
+                case OpCode.GotoNoStackPush:
+                    Goto(false);
+                    break;
+                case OpCode.GotoIf:
+                    if (ReadBool())
+                        Goto(false);
+                    break;
+                case OpCode.GotoIfNot:
+                    if (!ReadBool())
+                        Goto(false);
+                    break;
+                case OpCode.GoBack or OpCode.GotoAfterLoop:
+                    Return();
+                    break;
+                case OpCode.Assign:
+                    variables[ReadAddress()].SetValue(variables[ReadAddress()].GetValueUnsafe());
+                    break;
+                case OpCode.BinaryOperator:
+                    ICBValue b = variables[ReadAddress()].GetValueUnsafe();
+                    ICBValue a = variables[ReadAddress()].GetValueUnsafe();
+                    // FIXME, this could crash with a divide by zero exception
+                    PushV(a.ExecuteBinaryOperatorUnsafe(ReadT<TokenType>(), b));
+                    break;
+                case OpCode.CallNative:
+                    // push arg1
+                    // ...
+                    // push argn
+                    // push n
+                    // push function
+                    CBFunction function = ((CBFunctionValue)variables[ReadAddress()].GetValueUnsafe()).value;
+                    uint ArgsLength = ReadT<uint>(); // exist for ...params
+                    ICBValue[] arguments = new ICBValue[ArgsLength];
+                    for (int i = 0; i < ArgsLength; i++)
+                        arguments[i] = variables[ReadAddress()].GetValueUnsafe();
+                        // no need for call unsafe because native functions will do a checks anyways
+                        // TODO: it might be worth to implement for declared functions
+                    PushV(function.Call().Swap().ValueUnsafe());
+                    break;
+
                 default:
                     throw new InvalidOperationException($"unknown instruction ({byteCode[programCounter]})");
             }
         }
     }
 
+
+    // private int ReadInt() {
+    //     byte[] bytes= new byte[4];
+    //     for (int i = 0; i < 4; i++)
+    //         bytes[i] = Read();
+
+    //     if (BitConverter.IsLittleEndian)
+    //         Array.Reverse(bytes);
+    //     return BitConverter.ToInt32(bytes, 0);
+    // }
+
+    // this is me having fun
+    private Address ReadAddress() {
+        Span<byte> bytes= stackalloc byte[addressSizeInBytes];
+        for (int i = 0; i < addressSizeInBytes; i++)
+            bytes[i] = Read();
+
+        if (BitConverter.IsLittleEndian)
+            bytes.Reverse();
+        return MemoryMarshal.Read<Address>(bytes);
+    }
+
+    private bool ReadBool() => MemoryMarshal.Read<bool>([Read()]);
+
     private byte Read() {
         return byteCode[programCounter++];
+    }
+
+    private T ReadT<T>() where T : struct {
+        int size = Marshal.SizeOf(default(T)) / sizeof(byte);
+        Span<byte> bytes = stackalloc byte[size];
+
+        for (int i = 0; i < size; i++)
+            bytes[i] = Read();
+
+        if (BitConverter.IsLittleEndian)
+            bytes.Reverse();
+
+        return MemoryMarshal.Read<T>(bytes);
+    }
+
+    // current op code is push so the next one should be the variable
+    private void Push() {
+        stack.Push(variables[ReadAddress()].GetValueUnsafe());
+    }
+
+    private void PushV(ICBValue value) {
+        stack.Push(value);
+    }
+
+    private ICBValue Pop() => stack.Pop();
+
+    private void Goto(bool pushStack) {
+        if (pushStack)
+            gotoStack.Push(programCounter);
+        programCounter = ReadAddress();
+    }
+
+    private void Return() {
+        programCounter = gotoStack.Pop();
     }
 }
--- Scripts/Language/Chartbuild/WM/OpCode.cs
+++ Scripts/Language/Chartbuild/WM/OpCode.cs
@@ -2,4 +2,24 @@ namespace PCE.Chartbuild.Runtime;
 
 public enum OpCode : byte {
     Halt, // stop the program
+    Push, // push to stack
+    Pop, // pop from the stack
+    Goto, // set the program counter
+    // goto but it won't push the program counter into the goto stack (aka history)
+    GotoNoStackPush, // it is just the continue for loops
+    // this is for the break statement
+    GotoAfterLoop, // when entering the loop the next instruction after the loop is on the goto stack which gets popped (I can already feel the bugs)
+    GotoIf, // if, it wont't push to the goto stack
+    GotoIfNot, // else, ^same
+    // go to the previous position from before the previous goto
+    GoBack, // it is just the return in functions
+    Assign, // assign a value to a variable
+    BinaryOperator, // execute a binary operator (a, b, op is on the stack)
+    //user defined functions rely on goto but that cannot call native functions
+    // but native functions are also stored as cbfunctions which are stored here
+    CallNative, // cbfunction with an index
+    // I don't know what this does but auto complete hallucinated it and I took a liking to it
+    // oh... it's a java thing
+    // TODO, debug breakpoint
+    Ldc_I4_0,
 }
