f9f7d5dbd24e2bb5e6776159cde58cfc2868ca51
Q-roo <kovacs113istvan@gamil.com>
2024-10-12 21:12:18 +0900

    remove rest of the unused code

--- Scripts/Language/Chartbuild/ByteCode/ValueLink.cs
+++
@@ -1,9 +0,0 @@
-namespace PCE.Chartbuild.Runtime;
-
-using Address = ushort;
-
-public class ValueLink(ByteCodeChunk chunk, string name) {
-    public readonly ByteCodeChunk chunk = chunk;
-    public readonly string name = name;
-    public Address Address => chunk.Lookup(this);
-}
