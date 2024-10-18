246816843f065a73354f694c826105a822fa1bc0
Q-roo <kovacs113istvan@gamil.com>
2024-10-18 17:15:56 +0900

    use DisplayServer.FileDialogShow instead of FileDialog

--- Scripts/Scenes/Common/PathSelect.cs
+++ Scripts/Scenes/Common/PathSelect.cs
@@ -1,78 +1,54 @@
 using Godot;
-using PCE.Util;
 
 namespace PCE.Editor;
 
-public partial class PathSelect : Panel
-{
+public partial class PathSelect : Panel {
     public delegate void OnFileDialogClose();
     public event OnFileDialogClose FileDialogClose;
 
+    private readonly Callable OnFileDialogFinish;
+
+    public PathSelect() {
+        OnFileDialogFinish = Callable.From((bool status, string[] paths, int filterIndex) => {
+            // status is wether it wasn't cancelled
+            OnFileSelected(status ? paths[filterIndex] : string.Empty);
+            FileDialogClose?.Invoke();
+        });
+    }
+
     private string _title;
     [Export]
-    public string Title
-    {
+    public string Title {
         get => _title;
-        set
-        {
+        set {
             _title = value;
             if (titleLabel is not null)
                 titleLabel.Text = value;
         }
     }
     [Export] public string[] allowedFileExtensions;
-    public string SelectedPath {get; private set; }
+    public string SelectedPath { get; private set; }
 
     private Label titleLabel;
     private Label pathLabel;
     private TextureButton openFileDialogButton;
-    private FileDialog fileDialog;
 
-    public sealed override void _Ready()
-    {
+    public sealed override void _Ready() {
         titleLabel = GetNode<Label>("HBoxContainer/Title");
         pathLabel = GetNode<Label>("HBoxContainer/Panel/HBoxContainer/Path");
         openFileDialogButton = GetNode<TextureButton>("HBoxContainer/Panel/HBoxContainer/OpenDialog");
-        fileDialog = new() {
-            FileMode = FileDialog.FileModeEnum.OpenFile,
-            Access = FileDialog.AccessEnum.Filesystem,
-            UseNativeDialog = true,
-            CancelButtonText = "cancel",
-            OkButtonText = "select",
-            DialogHideOnOk = true,
-            DialogCloseOnEscape = true,
-            ForceNative = true,
-            Title = "Open a file",
-            InitialPosition = Window.WindowInitialPosition.CenterMainWindowScreen,
-            Transient = true,
-            TransientToFocused = true,
-            Exclusive = true,
-        };//GetNode<FileDialog>("FileDialog");
 
         titleLabel.Text = Title;
         openFileDialogButton.Pressed += OnOpenFileDialogButtonPressed;
-        fileDialog.FileSelected += OnFileSelected;
-        fileDialog.Canceled += () => FileDialogClose?.Invoke();
-        this.GetRootViewport().CallDeferred("add_child", fileDialog);
     }
 
-    private void OnFileSelected(string path)
-    {
+    private void OnFileSelected(string path) {
         SelectedPath = path;
         pathLabel.Text = path;
-        FileDialogClose?.Invoke();
     }
 
-
-    private void OnOpenFileDialogButtonPressed()
-    {
-        fileDialog.Filters = allowedFileExtensions;
-        // fileDialog.GetWindowId();
-        // DisplayServer.FileDialogShow("Open A file", "", "", false, DisplayServer.FileDialogMode.OpenFile, allowedFileExtensions, Callable.From((bool status, string[] paths, int filterIndex) => {
-        //     GD.Print(paths);
-        // }));
-        // fileDialog.UseNativeDialog = false;
-        fileDialog.Show();
+    private void OnOpenFileDialogButtonPressed() {
+        DisplayServer.FileDialogShow("Open A file", "", "", false, DisplayServer.FileDialogMode.OpenFile, allowedFileExtensions, OnFileDialogFinish);
     }
 
 }
